316ee2ceb1998046d6b105c242516bcb50de462a
Luke Johnson <Rikjimue@gmail.com>
2024-07-02 22:59:25 -0700

    Added smoketrails to car when drifting

--- Assets/Scripts/BasicCarController.cs
+++ Assets/Scripts/BasicCarController.cs
@@ -2,31 +2,27 @@ using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
 
+// Sources: https://www.youtube.com/watch?v=cqATTzJmFDY&t=1s
+
 public class BasicCarController : MonoBehaviour
 {
     public Rigidbody myRigidBody;
 
-    public float forwardAccel = 8f, reverseAccel = 4f, maxSpeed = 50f, turnStrength = 180, gravityForce = 10f, dragOnGround = 3f;
+    [SerializeField] private float forwardAccel = 8f, reverseAccel = 4f, maxSpeed = 80f, turnStrength = 180, maxWheelTurn = 25f, gravityForce = 10f, dragOnGround = 3f, groundRayLength = 0.45f, yOffset = 0.45f, driftThreshold = 30f, minDriftSpeed = 15f;
+    [SerializeField] private Transform groundRayPoint, leftFrontWheel, rightFrontWheel;
+    [SerializeField] private LayerMask ground;
+    [SerializeField] private GameObject _carParticleObject;
+    private ParticleSystemManager _particleManager;
+    
     private float speedInput, turnInput;
-    public float yOffset = 0.45f;
-    
-    private bool grounded;
-    
-    public LayerMask ground;
-    public float groundRayLength = 0.45f;
-    public Transform groundRayPoint;
-    
-    public Transform leftFrontWheel, rightFrontWheel;
-    public float maxWheelTurn = 25f;
-
-    public ParticleSystem[] dustTrail;
-    public float maxEmission = 25f;
-    private float emissionRate;
+    private bool grounded, isDrifting;
 
     // Start is called before the first frame update
     void Start()
     {
         myRigidBody.transform.parent = null;
+
+        _particleManager = _carParticleObject.GetComponent<ParticleSystemManager>();
     }
 
     // Update is called once per frame
@@ -62,25 +58,34 @@ public class BasicCarController : MonoBehaviour
             transform.rotation = Quaternion.FromToRotation(transform.up, hit.normal) * transform.rotation;
         }
 
-        emissionRate = 0;
+        ControlDriftEffect();
 
         if (grounded) {
             myRigidBody.drag = dragOnGround;
 
             if (Mathf.Abs(speedInput) > 0) {
                 myRigidBody.AddForce(transform.forward * speedInput);
-                
-                emissionRate = maxEmission;
             }
         }
+        
         else {
             myRigidBody.drag = 0.1f;
             myRigidBody.AddForce(Vector3.up * -gravityForce * 100f);
         }
+    }
 
-        foreach(ParticleSystem part in dustTrail) {
-            var emissionModule = part.emission;
-            emissionModule.rateOverTime = emissionRate;
+    private void ControlDriftEffect() {
+        if (myRigidBody.velocity.magnitude > minDriftSpeed && grounded) {
+            Vector3 wheelDirection = leftFrontWheel.TransformDirection(Vector3.forward);
+            Vector3 carVelocityDirection = myRigidBody.velocity.normalized;
+
+            float driftAngle = Vector3.Angle(wheelDirection, carVelocityDirection);
+
+            if (driftAngle > driftThreshold) {
+                _particleManager.StartDriftEffect();
+                return;
+            }
         }
+        _particleManager.StopDriftEffect();
     }
 }
